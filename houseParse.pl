25aa97625c1a0df587091a8e0facbb5783a2d7ce
David Leung <david.leung@ericsson.com>
2015-11-18 15:44:21 +0100

    Added logic to store info in HoH.

--- houseParse.pl
+++ houseParse.pl
@@ -1,14 +1,18 @@
 #!/usr/bin/perl
 
+use strict;
+use warnings;
+use Data::Dumper;
+
 my $input_file = $ARGV[0];
 my $file_string;
 
 open(my $fh, '<', $input_file) or die "cannot open file $input_file";
 {
-    local $/;
+    local $/ = undef;
     $file_string = <$fh>;
+    close($fh);
 }
-close($fh);
 
 $file_string =~ s/.*\<form\>(.*?)\<\/form\>.*/$1/s;
 
@@ -43,7 +47,7 @@ while ($file_string =~ s/(.*?)\<b\>(.*?)\<\/b\>(.*?)/$1 --- $3/s)
 
     $contact = $2;
 
-    print "$address\n\t$type\n\t$rent\n\t$numRooms\n\t$dateAvailable\n\t$contact\n";
+    #print "$address\n\t$type\n\t$rent\n\t$numRooms\n\t$dateAvailable\n\t$contact\n";
 
     $info_hash{"$address"}{'Type'} = $type;
     $info_hash{"$address"}{'Rent'} = $rent;
@@ -52,6 +56,19 @@ while ($file_string =~ s/(.*?)\<b\>(.*?)\<\/b\>(.*?)/$1 --- $3/s)
 
     # Make into a hash?
     $info_hash{"$address"}{'Contact'} = $contact;
+}
+
+#print Dumper(\%info_hash);
+
+my $index = 1;
+
+print "Entry:\tAddress\tType\tRent\tNumber of Rooms\tDate Available\tContact\n";
+
+for my $address (keys(%info_hash))
+{
+    print "$index:\t$address\t$info_hash{$address}{'Type'}\t$info_hash{$address}{'Rent'}\t$info_hash{$address}{'NumRooms'}\t$info_hash{$address}{'DateAvail'}\t$info_hash{$address}{'Contact'}\n";
+
+    $index++;
 }
 
 #print $file_string;
