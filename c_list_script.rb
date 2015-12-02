632e66bab7bc1c55496aa4afe9d0d536fffe8633
David Leung <david.leung@ericsson.com>
2015-12-02 16:04:30 -0500

    Finished modularization effort. TODO list at top of file.

--- c_list_script.rb
+++ c_list_script.rb
@@ -3,98 +3,150 @@ require 'mechanize'
 require 'csv'
 require 'pry-byebug'
 
+##### TODO #####
+#
+#   - Parameterize program
+#   - Include a warning that default website is used, if none passed to program
+#   - Introduce 'usage' method (private?)
+#   - Fix parameterization of 'save_results' method
+#   - Create an array for @search_form fields with strict index (e.g., fields = [ query, max_price, min_price, url, ... ] )
+#   - Use external file to map page elements to website
+#   - Use hash/external file to map form fields to website
+#   - Fix method comments (RE: Params/Returns)
+#   - Fix error strings in argument validation
+#   - Optional optimizations (e.g., limit to certain radius)
+
 ##### GLOBAL VARIABLES #####
 
-# Hard-coded address for scraping
-#   TODO: Change this to an argument-based variable
+# Hard-coded default address in case none provided
 ADDRESS = 'http://kingston.craigslist.ca/search/apa'
 
-# Array to hold each of the elements in .csv file.
-#   TODO: Determine what fields are wanted/available
-results = []
-results << ['Name', 'URL', 'Price', 'Location']
+##### SCRAPER CLASS #####
 
-##### METHODS #####
+class Scraper
 
-### --Initialize Mechanize-- ###
-#   Params: <none>
-#   Returns: Initialized Mechanize with preferred configuration
-###
-def init
-    # Mechanize library used for scraping results
-    scraper = Mechanize.new
+    def initialize(address=ADDRESS)
+        @scraper = init()
+        @website = address
+        @search_form = nil
+        @search_results = []
+    end ## initialize Method
 
-    # Mechanize setup to rate limit your scraping to once every half-second
-    scraper.history_added = Proc.new { sleep 0.5 }
-end
 
-### --Retrieve and fill form-- ###
-#   Params: address - URL where search form is located
-#   Returns: Completed search form
-#   NOTE: May need to pass more arguments for search queries
-###
-def fill_form(address=ADDRESS)
-    # Begin scraping
-    search_page = scraper.get(ADDRESS)
+    public
 
-    # Work with the form
-    form = search_page.form_with( :id => 'searchform' )
+    ### --Retrieve and fill form-- ###
+    #   Params: address - URL where search form is located
+    #           scraper - Previously initialized Mechanize object
+    #   Returns: Completed search form
+    #   NOTE: May need to pass more arguments for search queries
+    ###
+    #def fill_form(scraper, address=ADDRESS)
+    def fill_form
+        # Validate argument variables
+        raise "First argument of fill_form must be a Mechanize object" unless @scraper.respond_to?(:get)
 
-    #   TODO: Make query and other search parameters argument-based variables
-    #search.query = 'student'
-    #search.maxAsk = 0
-    # Others...?
-end
+        raise "Second argument of fill_form must be a String" unless @website.respond_to?(:to_str)
 
-### --Submit form and parse results-- ###
-#   Params: form - form to submit to get parsable results
-#   Returns: An array with results
-###
-def get_results(form)
-    # Submit form to get results
-    result_page = form.submit
+        # Begin scraping
+        search_page = @scraper.get(@website.clone)
 
-    # Get results
-    #   TODO: Determine unique identifier of search results
-    raw_results = result_page.search('p.row')
+        # Work with the form
+        form = search_page.form_with( :id => 'searchform' )
 
-    # Parse the results
-    #   TODO: Determine best way to parse each of the required fields
-    raw_results.each do |result|
-        link = result.css('a')[1]
-        name = link.text.strip
-        url = "http://kingston.craigslist.ca" + link.attributes["href"].value
-        price = result.search('span.price').text
-        location = result.search('span.pnr').text
+        #search.query = 'student'
+        #search.maxAsk = 0
+        # Others...?
 
-        #   TODO: Apply a filter based on street(?) to reduce number of postings
+        @search_form = form
+        self
+    end ## fill_form Method
 
-        # Save the results
-        results << [name, url, price, location]
-    end
-end
 
-### --Save files-- ###
-#   Params: *file - Any number of file names to save results to
-#   Returns: <none>
-###
-def save_results(*file)
-    CSV.open("test_output.csv", "w+") do |csv_file|
-        results.each do |row|
-            csv_file << row
+    ### --Submit form and parse results-- ###
+    #   Params: form - form to submit to get parsable results
+    #   Returns: An array with results
+    ###
+    #def get_results(form)
+    def get_results()
+        # Validate argument variables
+        raise "Argument of get_results must be a web-page form" unless @search_form.respond_to?(:submit)
+
+        # Submit form to get results
+        result_page = @search_form.submit
+
+        # Get results
+        raw_results = result_page.search('p.row')
+
+        # Array to hold each of the elements in .csv file.
+        results = []
+        results << ['Name', 'URL', 'Price', 'Location']
+
+        # Parse the results
+        raw_results.each do |result|
+            link = result.css('a')[1]
+            name = link.text.strip  # result.link.text.strip
+            url = "http://kingston.craigslist.ca" + link.attributes["href"].value   # result.link.attributes["href"].value
+            price = result.search('span.price').text
+            location = result.search('span.pnr').text
+
+            # Save the results
+            results << [name, url, price, location]
+        end
+
+        @search_results = results
+        self
+    end ## get_results Method
+
+    ### --Save files-- ###
+    #   Params: *file - Any number of file names to save results to
+    #           results - Array containing results to be saved
+    #   Returns: <none>
+    ###
+    #def save_results(results, *file)
+    def save_results(*file)
+        # Validate argument variables
+        raise "First argument of save_results must be an array" unless @search_results.respond_to?(:each)
+
+        CSV.open("test_output.csv", "w+") do |csv_file|
+            @search_results.each do |row|
+                csv_file << row
+            end
+        end
+
+        File.open("text_output.txt", "w+") do |txt_file|
+            @search_results.each do |row|
+                name = row[0]
+                url = row[1]
+                price = row[2]
+                location = row[3]
+                txt_file << "Name: #{name}\n\tURL: #{url}\n\tPrice: #{price}\n\tLocation: #{location}\n\n"
+            end
         end
     end
 
-    #   TODO: Save to a human readable format (.txt?) as well
-    File.open("text_output.txt", "w+") do |txt_file|
-        results.each do |row|
-            name = row[0]
-            url = row[1]
-            price = row[2]
-            location = row[3]
-            txt_file << "Name: #{name}\n\tURL: #{url}\n\tPrice: #{price}\n\tLocation: #{location}\n\n"
-        end
-    end
-end
+
+    private
+
+    ### --Initialize Mechanize-- ###
+    #   Params: <none>
+    #   Returns: Initialized Mechanize with preferred configuration
+    ###
+    def init
+        # Mechanize library used for scraping results
+        scraper = Mechanize.new
+
+        # Mechanize setup to rate limit your scraping to once every half-second
+        scraper.history_added = Proc.new { sleep 0.5 }
+
+        scraper
+    end ## init Method
+
+end ## Scraper Class
+
+##### MAIN #####
+
+c_list = Scraper.new()
+c_list.fill_form.get_results.save_results
 
 ##### END #####
