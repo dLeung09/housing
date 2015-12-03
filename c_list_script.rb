03dea1e9598240b9802aff86967efcb6ebe6d11b
David Leung <davidaleung@rogers.com>
2015-12-03 16:26:53 -0500

    Begin parameterizing program - enforcing filtered searches.

--- c_list_script.rb
+++ c_list_script.rb
@@ -2,19 +2,23 @@ require 'rubygems'
 require 'mechanize'
 require 'csv'
 require 'pry-byebug'
+require 'optparse'
+
 
 ##### TODO #####
 #
+#   - Create an array for @search_form fields with strict index (e.g., fields = [ query, max_price, min_price, url, ... ] )
+#       |-> In progress
 #   - Parameterize program
 #   - Include a warning that default website is used, if none passed to program
 #   - Introduce 'usage' method (private?)
 #   - Fix parameterization of 'save_results' method
-#   - Create an array for @search_form fields with strict index (e.g., fields = [ query, max_price, min_price, url, ... ] )
 #   - Use external file to map page elements to website
 #   - Use hash/external file to map form fields to website
 #   - Fix method comments (RE: Params/Returns)
 #   - Fix error strings in argument validation
-#   - Optional optimizations (e.g., limit to certain radius)
+#   - Optional: optimizations (e.g., limit to certain radius)
+#   - Optional: Add other configurations to 'init'
 
 ##### GLOBAL VARIABLES #####
 
@@ -29,6 +33,7 @@ class Scraper
         @scraper = init()
         @website = address
         @search_form = nil
+        @search_fields = []
         @search_results = []
     end ## initialize Method
 
@@ -41,7 +46,6 @@ class Scraper
     #   Returns: Completed search form
     #   NOTE: May need to pass more arguments for search queries
     ###
-    #def fill_form(scraper, address=ADDRESS)
     def fill_form
         # Validate argument variables
         raise "First argument of fill_form must be a Mechanize object" unless @scraper.respond_to?(:get)
@@ -51,12 +55,60 @@ class Scraper
         # Begin scraping
         search_page = @scraper.get(@website.clone)
 
-        # Work with the form
-        form = search_page.form_with( :id => 'searchform' )
+        # Work with the form. Used block to wrap all form fields in separate scope.
+        form = search_page.form_with( :id => 'searchform' ) do |search|
 
-        #search.query = 'student'
-        #search.maxAsk = 0
-        # Others...?
+            search.checkbox_with( :name => 'srchType' ).check
+            search.checkbox_with( :name => 'hasPic' ).check
+            search.checkbox_with( :name => 'postedToday' ).check
+            search.checkbox_with( :name => 'searchNearby' ).check
+
+            search.min_price = 0
+            search.max_price = 1000
+
+            search.field_with( :name => 'bedrooms' ).options[0].click
+            search.field_with( :name => 'bathrooms' ).options[1].click
+
+            search.minSqft = 0
+            search.maxSqft = 1000000
+
+            search.checkbox_with( :name => 'pets_cat' ).check
+            search.checkbox_with( :name => 'pets_dog' ).check
+            search.checkbox_with( :name => 'is_furnished' ).check
+            search.checkbox_with( :name => 'no_smoking' ).check
+            search.checkbox_with( :name => 'wheelchaccess' ).check
+
+            search.checkbox_with( :id => 'apartment_1' ).check
+            search.checkbox_with( :id => 'condo_2' ).check
+            search.checkbox_with( :id => 'cottage/cabin_3' ).check
+            search.checkbox_with( :id => 'duplex_4' ).check
+            search.checkbox_with( :id => 'flat_5' ).check
+            search.checkbox_with( :id => 'house_6' ).check
+            search.checkbox_with( :id => 'in-law_7' ).check
+            search.checkbox_with( :id => 'loft_8' ).check
+            search.checkbox_with( :id => 'townhouse_9' ).check
+            search.checkbox_with( :id => 'manufactured_10' ).check
+            search.checkbox_with( :id => 'assisted_living_11' ).check
+            search.checkbox_with( :id => 'land_12' ).check
+
+            search.checkbox_with( :id => 'w/d_in_unit_1' ).check
+            search.checkbox_with( :id => 'w/d_hookups_4' ).check
+            search.checkbox_with( :id => 'laundry_in_bldg_2' ).check
+            search.checkbox_with( :id => 'laundry_on_site_3' ).check
+            search.checkbox_with( :id => 'no_laundry_on_site_5' ).check
+
+            search.checkbox_with( :id => 'carport_1' ).check
+            search.checkbox_with( :id => 'attached_garage_2' ).check
+            search.checkbox_with( :id => 'detached_garage_3' ).check
+            search.checkbox_with( :id => 'off-street_parking_4' ).check
+            search.checkbox_with( :id => 'street_parking_5' ).check
+            search.checkbox_with( :id => 'valet_parking_6' ).check
+            search.checkbox_with( :id => 'no_parking_7' ).check
+
+            search.field_with( :id => 'sale_date' ).options[0].click
+
+            search.query = 'student'
+        end
 
         @search_form = form
         self
@@ -142,7 +194,80 @@ class Scraper
         scraper
     end ## init Method
 
+    ### --Parse Program Arguments-- ###
+    #   Params: <none>
+    #   Returns: <none>
+    ###
+    def parse_args
+        options = {}
+        OptionParser.new do |opt|
+            opt.on('', '--title-only', 'Search for query in title only') { |b| options[:title_only] = b }
+
+            opt.on('', '--has-pic', 'Only show results with a picture') { |b| options[:has_pic] = b }
+
+            opt.on('', '--posted-today', 'Only show results posted today') { |b| option[:posted_today] = b }
+
+            opt.on('', '--nearby', 'Only show nearby results') { |b| option[:nearby] = b }
+
+            opt.on('', '--min-price MIN_PRICE', 'Filter out results for less than MIN_PRICE') { |o| option[:min_price] = o }
+
+            opt.on('', '--max-price MAX_PRICE', 'Filter out results for more than MAX_PRICE') { |o| option[:max_price] = o }
+
+            opt.on('', '--bedrooms NUM', 'Filter out results with less than NUM bedrooms') { |o| option[:bedrooms] = o }
+
+            opt.on('', '--bathrooms NUM', 'Filter out results with less than NUM bathrooms') { |o| option[:bathrooms] = o }
+
+            opt.on('', '--min-sq-ft NUM', 'Filter out results with less than NUM square feet') { |o| option[:min_ft] = o }
+
+            opt.on('', '--max-sq-ft NUM', 'Filter out results with more than NUM square feet') { |o| option[:max_ft] = o }
+
+            opt.on('', '--allow-cat', 'Filter out results that do NOT allow cats') { |o| option[:pets_cat] = o }
+
+            opt.on('', '--allow-dog', 'Filter out results that do NOT allow dogs') { |o| option[:pets_dog] = o }
+
+            opt.on('', '--furnished', 'Filter out results that are NOT furnished') { |o| option[:furnished] = o }
+
+            opt.on('', '--no-smoking', 'Filter out results that allow smoking') { |o| option[:no_smoking] = o }
+
+            opt.on('', '--wheelchair', 'Filter out results that are NOT wheelchair accessible') { |o| option[:wheelchair] = o }
+
+            #search.checkbox_with( :id => 'apartment_1' ).check
+            #search.checkbox_with( :id => 'condo_2' ).check
+            #search.checkbox_with( :id => 'cottage/cabin_3' ).check
+            #search.checkbox_with( :id => 'duplex_4' ).check
+            #search.checkbox_with( :id => 'flat_5' ).check
+            #search.checkbox_with( :id => 'house_6' ).check
+            #search.checkbox_with( :id => 'in-law_7' ).check
+            #search.checkbox_with( :id => 'loft_8' ).check
+            #search.checkbox_with( :id => 'townhouse_9' ).check
+            #search.checkbox_with( :id => 'manufactured_10' ).check
+            #search.checkbox_with( :id => 'assisted_living_11' ).check
+            #search.checkbox_with( :id => 'land_12' ).check
+            #search.checkbox_with( :id => 'w/d_in_unit_1' ).check
+            #search.checkbox_with( :id => 'w/d_hookups_4' ).check
+            #search.checkbox_with( :id => 'laundry_in_bldg_2' ).check
+            #search.checkbox_with( :id => 'laundry_on_site_3' ).check
+            #search.checkbox_with( :id => 'no_laundry_on_site_5' ).check
+            #search.checkbox_with( :id => 'carport_1' ).check
+            #search.checkbox_with( :id => 'attached_garage_2' ).check
+            #search.checkbox_with( :id => 'detached_garage_3' ).check
+            #search.checkbox_with( :id => 'off-street_parking_4' ).check
+            #search.checkbox_with( :id => 'street_parking_5' ).check
+            #search.checkbox_with( :id => 'valet_parking_6' ).check
+            #search.checkbox_with( :id => 'no_parking_7' ).check
+            #search.field_with( :id => 'sale_date' ).options[0].click
+            opt.on('-q', '--query QUERY', 'Search for results that include QUERY') { |o| options[:query] = o }
+        end
+    end
+
 end ## Scraper Class
+
+##### OTHER METHODS #####
+#   Current best-thinking:
+#       - parse_args
+#       - usage
+#       - read_file
+
 
 ##### MAIN #####
 
